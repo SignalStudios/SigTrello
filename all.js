697f9263b8161ec8b98ae2ceeb31ee016eb942af
Michael B. E. Rickert <nobody@nowhere.noplace>
2014-05-10 15:53:07 -0700

    Add project, typemap, recompile after I forgot to when adding the SigTrello module.

--- all.js
+++ all.js
@@ -9,147 +9,151 @@
 ///<reference path='trello-client.d.ts'/>
 ///<reference path='chrome.d.ts'/>
 ///<reference path='mutation-observer.d.ts'/>
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver || null;
+var SigTrello;
+(function (SigTrello) {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver || null;
 
-function error(message) {
-    //alert( message );
-}
+    function error(message) {
+        //alert( message );
+    }
 
-var spamLimitCounter = 1000;
-function spamLimit() {
-    //return --limit < 0;
-    return false;
-}
+    var spamLimitCounter = 1000;
+    function spamLimit() {
+        //return --limit < 0;
+        return false;
+    }
 
-function getBestByNameIndex(name, index, items) {
-    if (items.length > index && items[index].name == name)
-        return items[index];
+    function getBestByNameIndex(name, index, items) {
+        if (items.length > index && items[index].name == name)
+            return items[index];
 
-    var matchingName = $.grep(items, function (i) {
-        return i.name == name;
-    }, false);
-    if (matchingName.length == 1)
-        return matchingName[0];
+        var matchingName = $.grep(items, function (i) {
+            return i.name == name;
+        }, false);
+        if (matchingName.length == 1)
+            return matchingName[0];
 
-    return null;
-}
+        return null;
+    }
 
-function authorize() {
-    Trello.authorize({
-        type: "popup",
-        name: "SigTrello",
-        persist: true,
-        interactive: true,
-        scope: { read: true, write: true, account: false },
-        expiration: "never"
-    });
-}
+    function authorize() {
+        Trello.authorize({
+            type: "popup",
+            name: "SigTrello",
+            persist: true,
+            interactive: true,
+            scope: { read: true, write: true, account: false },
+            expiration: "never"
+        });
+    }
 
-function getFlatName($node) {
-    var $elements = $node.contents();
-    var s = "";
-    for (var i = 0; i < $elements.length; ++i) {
-        var element = $elements.get(i);
-        if (element.nodeName == 'A') {
-            s += $(element).attr("href");
-        } else {
-            s += $(element).text();
+    function getFlatName($node) {
+        var $elements = $node.contents();
+        var s = "";
+        for (var i = 0; i < $elements.length; ++i) {
+            var element = $elements.get(i);
+            if (element.nodeName == 'A') {
+                s += $(element).attr("href");
+            } else {
+                s += $(element).text();
+            }
         }
-    }
-    return s;
-}
-
-function replaceWithLink() {
-    var toLink = this;
-    if (!Trello.authorized()) {
-        authorize();
-        if (!Trello.authorized())
-            return false;
+        return s;
     }
 
-    var $this = $(this);
+    function replaceWithLink() {
+        var toLink = this;
+        if (!Trello.authorized()) {
+            authorize();
+            if (!Trello.authorized())
+                return false;
+        }
 
-    var $checklistItem = $this.parent('.checklist-item');
-    var $checklistItemText = $checklistItem.find('.checklist-item-details-text');
-    var $links = $checklistItemText.find('a');
-    if ($links.length == 1 && $links.text == $this.text)
-        return;
+        var $this = $(this);
 
-    var baseURI = window.document.baseURI;
-    var cardShortId = baseURI.replace(/^http[s]?:\/\/trello.com\/c\/([^\/]+)\/.*/, "$1");
-    var $checklistItemsList = $checklistItem.parent('.checklist-items-list');
-    var $checklist = $checklistItemsList.parent('.checklist');
-    var $checklistList = $checklist.parent('.checklist-list');
+        var $checklistItem = $this.parent('.checklist-item');
+        var $checklistItemText = $checklistItem.find('.checklist-item-details-text');
+        var $links = $checklistItemText.find('a');
+        if ($links.length == 1 && $links.text == $this.text)
+            return;
 
-    var checklistItemTitle = $checklistItemText.text();
-    var checklistItemName = getFlatName($checklistItemText);
-    var checklistItemIndex = $checklistItemsList.find('.checklist-item').index($checklistItem);
-    var checklistTitle = $checklist.find('.checklist-title h3').text();
-    var checklistIndex = $checklistList.find('.checklist').index($checklist);
+        var baseURI = window.document.baseURI;
+        var cardShortId = baseURI.replace(/^http[s]?:\/\/trello.com\/c\/([^\/]+)\/.*/, "$1");
+        var $checklistItemsList = $checklistItem.parent('.checklist-items-list');
+        var $checklist = $checklistItemsList.parent('.checklist');
+        var $checklistList = $checklist.parent('.checklist-list');
 
-    $this.toggle();
+        var checklistItemTitle = $checklistItemText.text();
+        var checklistItemName = getFlatName($checklistItemText);
+        var checklistItemIndex = $checklistItemsList.find('.checklist-item').index($checklistItem);
+        var checklistTitle = $checklist.find('.checklist-title h3').text();
+        var checklistIndex = $checklistList.find('.checklist').index($checklist);
 
-    // TODO: Learn how to tame JavaScript callback hell.
-    // 1.  Identify the trello list
-    Trello.get("cards/" + cardShortId + "/list", { fields: "" }).done(function (list) {
-        // 2. Identify the checklist item being replaced.
-        Trello.get("cards/" + cardShortId + "/checklists").done(function (cardInfo) {
-            var cardChecklist = getBestByNameIndex(checklistTitle, checklistIndex, cardInfo);
-            if (cardChecklist == null) {
-                error("Checklist ambiguous or missing!");
-                return;
-            }
+        $this.toggle();
 
-            var cardChecklistItem = getBestByNameIndex(checklistItemName, checklistItemIndex, cardChecklist.checkItems);
-            if (cardChecklistItem == null) {
-                error("Checklist item ambiguous or missing!");
-                return;
-            }
+        // TODO: Learn how to tame JavaScript callback hell.
+        // 1.  Identify the trello list
+        Trello.get("cards/" + cardShortId + "/list", { fields: "" }).done(function (list) {
+            // 2. Identify the checklist item being replaced.
+            Trello.get("cards/" + cardShortId + "/checklists").done(function (cardInfo) {
+                var cardChecklist = getBestByNameIndex(checklistTitle, checklistIndex, cardInfo);
+                if (cardChecklist == null) {
+                    error("Checklist ambiguous or missing!");
+                    return;
+                }
 
-            // 3.  Post new card to the list
-            Trello.post("lists/" + list.id + "/cards", { name: checklistItemTitle, desc: "Parent: " + baseURI + " " + checklistTitle, due: null }).done(function (newCard) {
-                // 4.  Replace checklist item with new card
-                Trello.put("cards/" + cardShortId + "/checklist/" + cardChecklist.id + "/checkItem/" + cardChecklistItem.id + "/name", { value: newCard.url });
+                var cardChecklistItem = getBestByNameIndex(checklistItemName, checklistItemIndex, cardChecklist.checkItems);
+                if (cardChecklistItem == null) {
+                    error("Checklist item ambiguous or missing!");
+                    return;
+                }
+
+                // 3.  Post new card to the list
+                Trello.post("lists/" + list.id + "/cards", { name: checklistItemTitle, desc: "Parent: " + baseURI + " " + checklistTitle, due: null }).done(function (newCard) {
+                    // 4.  Replace checklist item with new card
+                    Trello.put("cards/" + cardShortId + "/checklist/" + cardChecklist.id + "/checkItem/" + cardChecklistItem.id + "/name", { value: newCard.url });
+                });
             });
         });
+
+        return false;
+    }
+
+    var newChecklistsObserver = new MutationObserver(function (mutations) {
+        var $checklistItemsList = $(".checklist-items-list .checklist-item");
+        for (var i = 0; i < $checklistItemsList.length; ++i) {
+            showConvertToCardButton($checklistItemsList.get(i));
+        }
+
+        var $checklistEditControls = $(".checklist-item-details .edit-controls");
+        if ($checklistEditControls.length > 0) {
+            showConvertToCardLink($checklistEditControls.get(0));
+        }
     });
 
-    return false;
-}
+    newChecklistsObserver.observe(document.body, { childList: true, characterData: false, attributes: false, subtree: true });
 
-var newChecklistsObserver = new MutationObserver(function (mutations) {
-    var $checklistItemsList = $(".checklist-items-list .checklist-item");
-    for (var i = 0; i < $checklistItemsList.length; ++i) {
-        showConvertToCardButton($checklistItemsList.get(i));
+    function showConvertToCardButton(location) {
+        if ($(location).find('.ctcButtonImg').length)
+            return;
+        if (spamLimit())
+            return;
+
+        // Space the checkbox farther from the linkify icon
+        $(location).find('.checklist-item-checkbox').css('left', '16px');
+
+        // Add clickable image to linkify checkbox
+        $("<img class='ctcButtonImg checklist-item-checkbox' style='left: -12px' src='" + chrome.extension.getURL('images/link.png') + "'>").appendTo(location).click(replaceWithLink);
     }
 
-    var $checklistEditControls = $(".checklist-item-details .edit-controls");
-    if ($checklistEditControls.length > 0) {
-        showConvertToCardLink($checklistEditControls.get(0));
+    function showConvertToCardLink(location) {
+        if ($(location).find('.js-convert-item-to-link').length)
+            return;
+        if (spamLimit())
+            return;
+
+        // Add link to checkbox additional options
+        $("<a href='#' class='option convert js-convert-item-to-link'>Convert to Link</a>").insertAfter($(location).find('.js-delete-item').get(0)).click(replaceWithLink);
     }
-});
-
-newChecklistsObserver.observe(document.body, { childList: true, characterData: false, attributes: false, subtree: true });
-
-function showConvertToCardButton(location) {
-    if ($(location).find('.ctcButtonImg').length)
-        return;
-    if (spamLimit())
-        return;
-
-    // Space the checkbox farther from the linkify icon
-    $(location).find('.checklist-item-checkbox').css('left', '16px');
-
-    // Add clickable image to linkify checkbox
-    $("<img class='ctcButtonImg checklist-item-checkbox' style='left: -12px' src='" + chrome.extension.getURL('images/link.png') + "'>").appendTo(location).click(replaceWithLink);
-}
-
-function showConvertToCardLink(location) {
-    if ($(location).find('.js-convert-item-to-link').length)
-        return;
-    if (spamLimit())
-        return;
-
-    // Add link to checkbox additional options
-    $("<a href='#' class='option convert js-convert-item-to-link'>Convert to Link</a>").insertAfter($(location).find('.js-delete-item').get(0)).click(replaceWithLink);
-}
+})(SigTrello || (SigTrello = {}));
+//# sourceMappingURL=all.js.map
